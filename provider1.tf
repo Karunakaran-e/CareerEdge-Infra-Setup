969d0796e3641006f1dca9166d3e9a8b1c8e681b
Karunakaran <135000016+Karunakaran-e@users.noreply.github.com>
2023-06-08 05:11:49 +0000

    Initial commit

---
+++ provider1.tf
@@ -0,0 +1,30 @@
+# Terraform Settings Block
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.70"
+      #version = ">= 4.65"
+    }
+    helm = {
+      source = "hashicorp/helm"
+      #version = "2.4.1"
+      #version = "~> 2.4"
+      version = "~> 2.9"
+    }
+
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      #version = "~> 2.7"
+      version = ">= 2.20"
+    }
+
+    http = {
+      source = "hashicorp/http"
+      #version = "2.1.0"
+      #version = "~> 2.1"
+      version = "~> 3.3"
+    }
+  }
+}
